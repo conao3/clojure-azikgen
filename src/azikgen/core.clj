4c713a502e77dcec2dad75fd7025466cd9755cef
conao3 <conao3@gmail.com>
2024-11-05 10:59:58 +0900

    add first test

--- src/azikgen/core.clj
+++ src/azikgen/core.clj
@@ -1,9 +1,21 @@
 (ns azikgen.core
   (:require
+   [clj-yaml.core :as yaml]
+   [clojure.data.json :as json]
+   [clojure.java.io :as io]
    [clojure.tools.logging :as log])
   (:gen-class))
+
+(defn generate [rules defs]
+  rules)
 
 (defn -main
   "The entrypoint."
   [& args]
-  (log/info args))
+  (log/info args)
+  (-> (first args)
+      io/reader
+      yaml/parse-stream
+      (->> (generate nil))
+      json/json-str
+      println))
--- test/azikgen/core_test.clj
+++ test/azikgen/core_test.clj
@@ -1,6 +1,10 @@
 (ns azikgen.core-test
   (:require
-   [clojure.test :as t]))
+   [clojure.test :as t]
+   [azikgen.core :as sut]))
 
 (t/deftest sample-test
   (t/is (= true true)))
+
+(t/deftest generate-test
+  (t/is (= nil (sut/generate nil nil))))
