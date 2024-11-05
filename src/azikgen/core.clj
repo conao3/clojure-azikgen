fbddb8aaa5f27d3aae441d14ea885042d6e91f07
conao3 <conao3@gmail.com>
2024-11-05 14:33:59 +0900

    add dependent-macro-rule

--- src/azikgen/core.clj
+++ src/azikgen/core.clj
@@ -6,6 +6,17 @@
    [clojure.tools.logging :as log])
   (:gen-class))
 
+(defn apply-rule [rules inpt]
+  (let [len (count inpt)]
+    (when (seq inpt)
+      (or
+       (and (<= 1 len) (when-let [t (get rules (subs inpt 0 1))] (cons t (apply-rule rules (subs inpt 1)))))
+       (and (<= 2 len) (when-let [t (get rules (subs inpt 0 2))] (cons t (apply-rule rules (subs inpt 2)))))
+       (and (<= 3 len) (when-let [t (get rules (subs inpt 0 3))] (cons t (apply-rule rules (subs inpt 3)))))
+       (and (<= 4 len) (when-let [t (get rules (subs inpt 0 4))] (cons t (apply-rule rules (subs inpt 4)))))
+       (and (<= 5 len) (when-let [t (get rules (subs inpt 0 5))] (cons t (apply-rule rules (subs inpt 5)))))
+       (throw (ex-info (format "Cannot interpret %s using current rule set" inpt) {}))))))
+
 (defn handler-matrix-rule [step]
   (->> (mapcat (fn [[key val]]
                  (map
@@ -25,13 +36,25 @@
   (-> (:matrix step)
       (update-keys name)))
 
+(defn handler-dependent-macro-rule [rules step]
+  (->> (for [p (:pre step)
+             [mk mv] (:macro step)]
+         (when-let [applied (try
+                              (apply-rule rules (str p mv))
+                              (catch Exception e
+                                (log/warn (ex-message e))
+                                nil))]
+           [(str p (name mk)) (apply str applied)]))
+       (into {})))
+
 (defn generate-1 [rules step]
   (merge
    rules
    (condp = (:type step)
      "matrix-rule" (handler-matrix-rule step)
      "postfix-rule" (handler-postfix-rule step)
-     "raw-rule" (handler-raw-rule step))))
+     "raw-rule" (handler-raw-rule step)
+     "dependent-macro-rule" (handler-dependent-macro-rule rules step))))
 
 (defn generate
   ([steps]
--- test/azikgen/core_test.clj
+++ test/azikgen/core_test.clj
@@ -52,3 +52,47 @@
   (t/is (= {"fu" "ふ"}
            (sut/handler-raw-rule {:type "raw-rule"
                                   :matrix {:fu "ふ"}}))))
+
+(t/deftest handler-dependent-macro-rule-test
+  (t/is (= {} (sut/handler-dependent-macro-rule nil nil)))
+
+  (t/is (= {"kz" "かん"
+            "kn" "かん"
+            "kk" "きん"
+            "kj" "くん"
+            "kd" "けん"
+            "kl" "こん"
+            "sz" "さん"
+            "sn" "さん"
+            "sk" "しん"
+            "sj" "すん"
+            "sd" "せん"
+            "sl" "そん"}
+           (sut/handler-dependent-macro-rule
+            {"ka" "か"
+             "ki" "き"
+             "ku" "く"
+             "ke" "け"
+             "ko" "こ"
+             "sa" "さ"
+             "si" "し"
+             "su" "す"
+             "se" "せ"
+             "so" "そ"
+             "nn" "ん"}
+            {:type "dependent-macro-rule"
+             :macro {:z "ann" :n "ann" :k "inn" :j "unn" :d "enn" :l "onn"}
+             :pre ["k", "s"]}))))
+
+(t/deftest apply-rule-test
+  (let [rules {"a" "あ"
+               "i" "い"
+               "ka" "か"}]
+    (t/is (= ["あ"]
+             (sut/apply-rule rules "a")))
+
+    (t/is (= ["か" "い"]
+             (sut/apply-rule rules "kai")))
+
+    (t/is (thrown? Exception
+           (sut/apply-rule rules "o")))))
